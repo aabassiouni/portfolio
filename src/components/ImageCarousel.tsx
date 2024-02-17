6c34f5078128731630463390e23f91881a6905a8
Ali Bassiouni <aabassiouni@hotmail.com>
2024-02-17 16:52:30 +0200

    style: move carousel buttons below card

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -4,17 +4,21 @@ import { Carousel, CarouselContent, CarouselItem, CarouselPrevious, CarouselNext
 
 function ImageCarousel({ children }: { children: React.ReactNode[] }) {
 	return (
-		<Card className="">
-			<CardContent className="pt-6">
-				<Carousel className="w-full h-full">
+		<Carousel opts={{
+			loop: true,
+		}} className="w-full h-full">
+			<Card className="">
+				<CardContent className="pt-6">
 					<CarouselContent className="items-center">
 						{children?.map((child, index) => <CarouselItem key={index}>{child}</CarouselItem>)}
 					</CarouselContent>
-					<CarouselPrevious className="text-white" />
-					<CarouselNext className="text-white" />
-				</Carousel>
-			</CardContent>
-		</Card>
+				</CardContent>
+			</Card>
+			<div className="pt-4 flex justify-center gap-8">
+				<CarouselPrevious className="text-white relative " />
+				<CarouselNext className="text-white relative" />
+			</div>
+		</Carousel>
 	);
 }
 
